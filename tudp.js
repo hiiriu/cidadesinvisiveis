5953ad6bc8cacbaa06fa9274ffca7bd4e12e1ef7
pirilaw <165023939+pirilaw@users.noreply.github.com>
2025-05-06 16:27:11 +0100

    updatelayout

--- tudp.js
+++ tudp.js
@@ -1,5 +1,7 @@
-let arrayCidades= ["",];
-let arrayVideos
+/* ---CIDADES ORGANIZADAS POR TOPICO -- ORGANIZACAO LIVRO I+4 A NAO SER QUE I SEJA 0 ENT I+1 OU I LENGTH --- */
+let arrayCidades= ["Diomira","Isidora","Zaira","Zora","Maurilia","Dorotéia","Anastásia","Despina","Fedora","Zobeide","Tamara","Zirma","Zoe","Ipásia","Olivia","Isaura","Zenóbia","Armilia","Sofrônia","Octavia","Eufêmia","Chloe","Eutrópia","Ersilia","Esmeraldina","Valdrada","Zemrude","Baucis","Fílide","Moriana","Aglaura","Leandra","Pyrrha","Clarice","Irene","Melânia","Adelma","Eusápia","Argia","Zaudomia","Euxódia","Beersheba","Tecla","Perinzia","Ândria","Leônia","Trude","Procópia","Cecilia","Penthesilea","Olinda","Raissa","Marozia","Theodora","Berenice"];
+
+
 let botao_play= document.getElementById("botao_pausa");
 let video= document.querySelector("video");
 let vid_paused = false;
